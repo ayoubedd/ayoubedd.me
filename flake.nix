25a281400e8c06ea26153b9035c399a034d6fc76
Ayoub Eddaoudi <me@ayoubedd.me>
2024-06-30 02:22:25 +0100

    chore: use flake-utils lib to populate diff arch confs

--- flake.nix
+++ flake.nix
@@ -3,21 +3,12 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { nixpkgs, flake-utils, ... }:
-    let
-      forAllSystems = f: builtins.listToAttrs (map (system: { name = system; value = f system; }) flake-utils.lib.defaultSystems);
-    in
-    {
-      devShells = forAllSystems (system: {
-        default = import ./shell.nix {
-          pkgs = import nixpkgs { inherit system; };
-        };
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+        devShell = import ./shell.nix { inherit pkgs; };
       });
-
-      formatter = forAllSystems (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in
-        pkgs.nixpkgs-fmt
-      );
-    };
 }
